d6ea5b6b1dc9b5576acb6e344df1a91260ad5f49
Aayush Naik <aayushnaik@Aayushs-MacBook-Pro.local>
2021-03-02 11:59:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import board
+import busio
+from digitalio import DigitalInOut
+from gpiozero import Buzzer
+from time import sleep
+
+from adafruit_pn532.i2c import PN532_I2C
+# from adafruit_pn532.spi import PN532_SPI
+# from adafruit_pn532.uart import PN532_UART
+
+# I2C connection:
+i2c = busio.I2C(board.SCL, board.SDA)
+
+reset_pin = DigitalInOut(board.D6)
+req_pin = DigitalInOut(board.D12)
+pn532 = PN532_I2C(i2c, debug=False, reset=reset_pin, req=req_pin)
+
+ic, ver, rev, support = pn532.firmware_version
+print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+
+# Configure PN532 to communicate with MiFare cards
+pn532.SAM_configuration()
+
+print("Waiting for RFID/NFC card...")
+while True:
+    # Check if a card is available to read
+    uid = pn532.read_passive_target(timeout=0.5)
+    print(".", end="")
+    # Try again if no card is available.
+    if uid is None:
+        continue
+    print("Found card with UID:", [hex(i) for i in uid])
+    buzzer.beep()
+    time.sleep(2)
